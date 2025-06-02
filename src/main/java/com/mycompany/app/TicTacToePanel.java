5ce48ad3baa2c305b2fc22b055e442d5d138b681
Matthewsoul124 <132999776+Matthewsoul124@users.noreply.github.com>
2025-06-02 18:48:18 +0300

    fix w

--- src/main/java/com/mycompany/app/TicTacToePanel.java
+++ src/main/java/com/mycompany/app/TicTacToePanel.java
@@ -3,68 +3,86 @@ package com.mycompany.app;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Arrays;
 
 public class TicTacToePanel extends JPanel implements ActionListener {
     private Game game;
     private TicTacToeCell[] cells = new TicTacToeCell[9];
 
-    private void createCell(int num, int x, int y) {
-        cells[num] = new TicTacToeCell(num, x, y);
-        cells[num].addActionListener(this);
-        add(cells[num]);
-    }
-
     public TicTacToePanel(GridLayout layout) {
         super(layout);
-        createCell(0, 0, 0);
-        createCell(1, 1, 0);
-        createCell(2, 2, 0);
-        createCell(3, 0, 1);
-        createCell(4, 1, 1);
-        createCell(5, 2, 1);
-        createCell(6, 0, 2);
-        createCell(7, 1, 2);
-        createCell(8, 2, 2);
+        initializeGame();
+        createCells();
+    }
+
+    private void initializeGame() {
         game = new Game();
         game.cplayer = game.player1;
     }
 
-    public void actionPerformed(ActionEvent ae) {
-        game.player1.move = -1;
-        game.player2.move = -1;
-
-        int i = 0;
-        for (TicTacToeCell jb : cells) {
-            if (ae.getSource() == jb) {
-                jb.setMarker(Character.toString(game.cplayer.symbol));
-            }
-            game.board[i++] = jb.getMarker();
-        }
-
-        if (game.cplayer == game.player1) {
-            game.player2.move = game.MiniMax(game.board, game.player2);
-            game.nmove = game.player2.move;
-            game.symbol = game.player2.symbol;
-            game.cplayer = game.player2;
-            if (game.player2.move > 0)
-                cells[game.player2.move - 1].doClick();
-        } else {
-            game.nmove = game.player1.move;
-            game.symbol = game.player1.symbol;
-            game.cplayer = game.player1;
-        }
-
-        game.state = game.checkState(game.board);
-
-        if (game.state == State.XWIN) {
-            JOptionPane.showMessageDialog(null, "Выиграли крестики", "Результат", JOptionPane.WARNING_MESSAGE);
-            System.exit(0);
-        } else if (game.state == State.OWIN) {
-            JOptionPane.showMessageDialog(null, "Выиграли нолики", "Результат", JOptionPane.WARNING_MESSAGE);
-            System.exit(0);
-        } else if (game.state == State.DRAW) {
-            JOptionPane.showMessageDialog(null, "Ничья", "Результат", JOptionPane.WARNING_MESSAGE);
-            System.exit(0);
+    private void createCells() {
+        for (int i = 0; i < 9; i++) {
+            int row = i / 3;
+            int col = i % 3;
+            cells[i] = new TicTacToeCell(i, col, row);
+            cells[i].addActionListener(this);
+            add(cells[i]);
         }
     }
-} 
+
+    public void actionPerformed(ActionEvent e) {
+        TicTacToeCell clickedCell = (TicTacToeCell) e.getSource();
+        
+        if (clickedCell.getMarker() != ' ') return;
+        
+        clickedCell.setMarker(String.valueOf(game.cplayer.symbol));
+        updateGameBoard();
+        
+        if (game.cplayer == game.player1) {
+            makeAIMove();
+        }
+        
+        checkGameState();
+    }
+
+    private void updateGameBoard() {
+        for (int i = 0; i < 9; i++) {
+            game.board[i] = cells[i].getMarker();
+        }
+    }
+
+    private void makeAIMove() {
+        game.player2.move = game.MiniMax(game.board, game.player2);
+        if (game.player2.move > 0) {
+            cells[game.player2.move - 1].setMarker(String.valueOf(game.player2.symbol));
+            updateGameBoard();
+        }
+    }
+
+    private void checkGameState() {
+        game.state = game.checkState(game.board);
+        game.symbol = game.cplayer.symbol;
+
+        if (game.state == State.XWIN) {
+            JOptionPane.showMessageDialog(this, "X wins!", "Game Over", JOptionPane.INFORMATION_MESSAGE);
+            resetGame();
+        } else if (game.state == State.OWIN) {
+            JOptionPane.showMessageDialog(this, "O wins!", "Game Over", JOptionPane.INFORMATION_MESSAGE);
+            resetGame();
+        } else if (game.state == State.DRAW) {
+            JOptionPane.showMessageDialog(this, "Draw!", "Game Over", JOptionPane.INFORMATION_MESSAGE);
+            resetGame();
+        } else {
+            game.cplayer = (game.cplayer == game.player1) ? game.player2 : game.player1;
+        }
+    }
+
+    private void resetGame() {
+        for (TicTacToeCell cell : cells) {
+            cell.setMarker(" ");
+        }
+        Arrays.fill(game.board, ' ');
+        game.cplayer = game.player1;
+        game.state = State.PLAYING;
+    }
+}
